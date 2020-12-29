213f67ecae15ff1974201084c3d0828c27b48c35
Tegan Sims <tegan@engaging.tech>
2020-12-29 18:21:49 +0000

    About screen

--- src/features/about/components/AboutComponent.js
+++ src/features/about/components/AboutComponent.js
@@ -1,35 +1,78 @@
 import React from "react"
 import { Title } from "react-native-paper"
-import { View } from "react-native"
+import { Text, StyleSheet, Linking, ScrollView } from "react-native"
 import BaseLayout from "../../../layouts/BaseLayout"
-import { useFonts, Karla_700Bold } from "@expo-google-fonts/karla"
 
-const AboutComponent = ({ handleSubmit, navigation }) => {
-  // let [fontsLoaded] = useFonts({
-  //   Karla_700Bold
-  // })
+const services = [
+  "All 4",
+  "Amazon Instant",
+  "Amazon Prime",
+  "BBC Iplayer",
+  "Google Play",
+  "iTunes",
+  "ITV Hub",
+  "My 5",
+  "Netflix",
+  "Now TV",
+  "Rakuten TV",
+  "TalkTalk TV Store"
+]
 
-  // if (!fontsLoaded) return null
+const LinkedText = ({ link, children }) => (
+  <Text
+    style={{ ...styles.text, fontFamily: "Karla_700Bold" }}
+    onPress={() => Linking.openURL(link)}
+  >
+    {children}
+  </Text>
+)
 
-  return (
-    <BaseLayout>
-      <View justifyContent="center" flex={1}>
-        <Title
-          style={{
-            fontFamily: "Karla_700Bold",
-            fontSize: 38,
-            textAlign: "center",
-            paddingTop: 24,
-            // paddingBottom: 24,
-            lineHeight: 50,
-            color: "white"
-          }}
-        >
-          About this app
-        </Title>
-      </View>
-    </BaseLayout>
-  )
-}
+const AboutComponent = () => (
+  <BaseLayout>
+    <ScrollView justifyContent="center" flex={1}>
+      <Title
+        style={{
+          fontFamily: "Karla_700Bold",
+          fontSize: 38,
+          textAlign: "center",
+          paddingTop: 48,
+          paddingBottom: 24,
+          lineHeight: 50,
+          color: "white"
+        }}
+      >
+        About this app
+      </Title>
+      <Text style={styles.text}>
+        Utilizing both the
+        <LinkedText link="https://rapidapi.com/utelly/api/utelly/details">
+          {" "}
+          Utelly{" "}
+        </LinkedText>
+        and
+        <LinkedText link="https://rapidapi.com/apidojo/api/imdb8/details">
+          {" "}
+          IMDb{" "}
+        </LinkedText>
+        APIs, this app searches across multiple Video on Demand services to let
+        you know exactly where you can watch that thing you wanted to watch
+        tonight!
+      </Text>
+      <Text style={styles.text}>Services this app searches include:</Text>
+      {services.map(service => (
+        <Text key={service} style={{ ...styles.text, paddingLeft: 12 }}>
+          • {service}
+        </Text>
+      ))}
+    </ScrollView>
+  </BaseLayout>
+)
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+    paddingBottom: 12
+  }
+})
 
 export default AboutComponent
--- src/features/about/stack/AboutStack.js
+++ src/features/about/stack/AboutStack.js
@@ -1,39 +1,16 @@
 import React from "react"
 import { createStackNavigator } from "@react-navigation/stack"
-import { View, Button } from "react-native"
-import { Text } from "react-native-paper"
 import AboutComponent from "../components/AboutComponent"
 
 const AboutStack = createStackNavigator()
 
-const ContactList = ({ navigation }) => (
-  <View>
-    <Text>A ContactList</Text>
-    <Button
-      onPress={() => navigation.navigate("SingleContact")}
-      title="To A Single Contact!"
-    />
-  </View>
-)
-
-const SingleContact = () => (
-  <View>
-    <Text>A Single Contact</Text>
-  </View>
-)
-
 const About = () => (
   <AboutStack.Navigator>
     <AboutStack.Screen
-      name="ContactList"
+      name="About"
       component={AboutComponent}
-      options={{ tabBarLabel: "ContactList!" }}
     />
-    <AboutStack.Screen
-      name="SingleContact"
-      component={SingleContact}
-      options={{ tabBarLabel: "SingleContact!" }}
-    />
+    
   </AboutStack.Navigator>
 )
 
